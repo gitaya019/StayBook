9c9abbf3aea0ab62abc446055cc23627f0e8c757
gitaya019 <ayalajhoan24@gmail.com>
2024-06-22 21:51:56 -0500

    delete 404.html

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/StayBook/",
+  base: "https://gitaya019.github.io/StayBook",
   plugins: [react()],
 })
